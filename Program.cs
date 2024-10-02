c0b3c32cb9d777f436449b96272f861fb12a135d
Dan Collins <dan.collins.dev@gmail.com>
2024-10-02 15:13:53 -0400

    Obligatory Hello World output

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program 
+    { 
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World! This is the beginning of my TodoApp written in C#!");
+        }
+    }
+}
